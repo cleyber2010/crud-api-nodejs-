730c5cf6c83380913c4add7c128d738e0171a79c
Cleyber <cleyber.fernandes@gmail.com>
2025-02-18 14:22:07 -0300

    api routes

---
+++ src/routes.js
@@ -0,0 +1,30 @@
+export const routes = [
+    {
+        method: 'GET',
+        path: '/users',
+        handler: (req, res) => {
+
+        }
+    },
+    {
+        method: 'POST',
+        path: '/users',
+        handler: (req, res) => {
+
+        }
+    },
+    {
+        method: 'PUT',
+        path: '/users/:id',
+        handler: (req, res) => {
+
+        }
+    },
+    {
+        method: 'DELETE',
+        path: '/users/:id',
+        handler: (req, res) => {
+
+        }
+    }
+];
