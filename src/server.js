7fb3ff77d870e686d48354ba1d6ce31f4769a7f3
Cleyber <cleyber.fernandes@gmail.com>
2025-02-18 14:21:54 -0300

    capturing routes

--- src/server.js
+++ src/server.js
@@ -1,7 +1,27 @@
 import http from 'node:http'
+import {routes} from "./routes.js";
 
-const server = http.createServer((req, res) => {
-    res.end("Hello World");
+const server = http.createServer(async (req, res) => {
+    const { method, url } = req;
+    const buffers = [];
+
+    for await (let chunck of req) {
+        buffers.push(chunck);
+    }
+
+    try {
+        req.body = JSON.parse(Buffer.concat(buffers).toString());
+    } catch {
+        req.body = null;
+    }
+
+    const route = routes.filter(item => item.method === method && item.path === url);
+
+    if (route) {
+        console.log(route);
+    }
+
+    res.writeHead(200).end();
 });
 
 server.listen(3333);
